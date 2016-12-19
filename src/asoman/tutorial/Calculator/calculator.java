26682210f1c7e39412fe96f1433adf2f43fc9846
ashut <ashut@10.0.0.241>
2016-12-18 22:29:07 -0500

    added calculator  program

--- src/asoman/tutorial/Calculator/calculator.java
+++ src/asoman/tutorial/Calculator/calculator.java
@@ -3,8 +3,31 @@ package asoman.tutorial.Calculator;
 public class calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+//		int i = Integer.parseInt(args[0]);
+//		int j = Integer.parseInt(args[1]);
+//		String k = args[2];	
+		
+		int i = 3;
+		int j = 4;
+		
+		String k  = "-";
+		
+		
+		if ( k == "+"){
+				
+				System.out.println(" the addition is = "+ (i+j));
+		              }
+		else if(k == "-")
+		{
+			
+			System.out.println(" the subtraction is = " + (i-j));
+			
+		}
+		
+	    	
+		
 	}
 
 }
